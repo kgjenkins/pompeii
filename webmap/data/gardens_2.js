94115c72b7b7d4c6728b07e90c12acf850517e50
Keith Jenkins <kgjenkins@gmail.com>
2019-03-08 18:32:26 -0500

    webmap files edited from qgis2web

---
+++ webmap/data/gardens_2.js
@@ -0,0 +1,9 @@
+var json_gardens_2 = {
+"type": "FeatureCollection",
+"name": "gardens_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "pompeii_id": "VIII.3.4", "name_local": "Casa di Ercole ed Auge", "name_english": "House of the Popidii", "pdf": "https:\/\/kgjenkins.github.io\/pompeii\/pdf\/Pompeii-VIII.3.pdf" }, "geometry": { "type": "Point", "coordinates": [ 14.485684689657779, 40.749171842541649 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "pompeii_id": "VII.2.1", "name_local": null, "name_english": "House of Championnet I", "pdf": "https:\/\/kgjenkins.github.io\/pompeii\/pdf\/Pompeii-VIII.2.pdf" }, "geometry": { "type": "Point", "coordinates": [ 14.484541433887728, 40.74858629727327 ] } }
+]
+}
